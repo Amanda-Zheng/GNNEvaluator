a3fc4dee9d7cc7b2a133c1e7b7f2de533d8d18dc
Hugh-Luo <hugh.luo599@gmail.com>
2023-01-19 15:04:45 +1100

    feat: trained model GIN (step 3): got meta-set records

--- auto-eval-gnn-master/sh_auto-eval-main-0119.sh
+++ auto-eval-gnn-master/sh_auto-eval-main-0119.sh
@@ -1,3 +1,4 @@
+:"
 # step 1
 CUDA_VISIBLE_DEVICES=1 python pretrain_gnn.py --seed=0 --source acm --target dblp \
 --lr 5e-3 --wd 1e-5 --epochs 200 --model GIN --full_s 0 --hid_dim 128 --encoder_dim 16
@@ -6,21 +7,21 @@ CUDA_VISIBLE_DEVICES=1 python pretrain_gnn.py --seed=0 --source acm --target dbl
 #2023-01-19 09:08:52,009 Namespace(encoder_dim=16, epochs=200, full_s=0, hid_dim=128, lr=0.005, model='GIN', seed=0, source='acm', target='dblp', wd=1e-05)
 #2023-01-19 09:08:52,010 Finish!, this is the log dir = ./logs/acm-to-dblp-GIN-full-0-0-20230119-090839-975822
 
-:"
 # step 2
 CUDA_VISIBLE_DEVICES=1 python meta_set_save.py --seed=0 --source acm --num_metas 300
 # --> get aug_data_path
 #./logs/Meta-save-acm-num-300-0-20230117-162906-177221
 "
 
-:"
 # step 3
 CUDA_VISIBLE_DEVICES=1 python meta_feat_acc.py --seed=0 --source acm \
 --num_metas 300 --model GIN --hid_dim 128 --encoder_dim 16 \
---model_path '$??' \
+--model_path './logs/acm-to-dblp-GIN-full-0-0-20230119-090839-975822' \
 --aug_data_path './logs/Meta-save-acm-num-300-0-20230117-162906-177221' # use meta-set we have created
 # --> get load_path
+#./logs/Meta-feat-acc-acm-GIN-num-300-0-20230119-091315-017978
 
+:"
 # step 4
 CUDA_VISIBLE_DEVICES=1 python meta_regression_nn.py --seed=0 \
 	--source acm --target dblp --model GIN --hid_dim 128 --encoder_dim 16 --val_num 30 --reg_model 'mlp_simple'\
