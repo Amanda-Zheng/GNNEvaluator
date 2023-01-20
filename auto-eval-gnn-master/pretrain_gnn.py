fd432db6ea12687b04af77d65a259f08367d362e
Hugh-Luo <hugh.luo599@gmail.com>
2023-01-20 12:24:34 +1100

    feat: trained model MLP (step 1): pretrained the model

--- auto-eval-gnn-master/pretrain_gnn.py
+++ auto-eval-gnn-master/pretrain_gnn.py
@@ -50,8 +50,8 @@ def main(args, device):
         encoder = GIN(source_data.num_node_features, hidden_channels=args.hid_dim, out_channels=args.encoder_dim,
                       num_layers=2).to(device)
     elif args.model == 'MLP':
-        encoder = MLP(source_data.num_node_features, hidden_channels=args.hid_dim, out_channels=args.encoder_dim,
-                      num_layers=2).to(device)
+        encoder = MLP(channel_list=[source_data.num_node_features,args.hid_dim, args.encoder_dim]).to(device)
+
 
     cls_model = nn.Sequential(nn.Linear(args.encoder_dim, dataset_s.num_classes), ).to(device)
 
@@ -90,7 +90,12 @@ def main(args, device):
 def test(data, models, encoder, cls_model, mask=None):
     for model in models:
         model.eval()
-    emb_out = encoder(data.x, data.edge_index)
+
+    if isinstance(encoder, MLP):
+        emb_out = encoder(data.x)
+    else: 
+        emb_out = encoder(data.x, data.edge_index)
+
     logits = cls_model(emb_out) if mask is None else cls_model(emb_out)[mask]
     preds = logits.argmax(dim=1)
     labels = data.y if mask is None else data.y[mask]
@@ -103,7 +108,11 @@ def train(models, encoder, cls_model, optimizer, loss_func, source_data):
     for model in models:
         model.train()
     s_train_mask = source_data.train_mask.to(torch.bool)
-    emb_source = encoder(source_data.x, source_data.edge_index)
+    if isinstance(encoder, MLP):
+        emb_source = encoder(source_data.x)
+    else: 
+        emb_source = encoder(source_data.x, source_data.edge_index)
+
     source_logits = cls_model(emb_source)
 
     if args.full_s == 1:
---
+++ auto-eval-gnn-master/sh_auto-eval-main-0119-v2.sh
@@ -0,0 +1,37 @@
+# step 1
+CUDA_VISIBLE_DEVICES=1 python pretrain_gnn.py --seed=0 --source acm --target dblp \
+  --lr 5e-3 --wd 1e-5 --epochs 200 --model MLP --full_s 0 --hid_dim 128 --encoder_dim 16
+# --> get model_path
+#./logs/acm-to-dblp-MLP-full-0-0-20230119-222614-746470
+
+:"
+# step 2
+CUDA_VISIBLE_DEVICES=1 python meta_set_save.py --seed=0 --source acm --num_metas 300
+# --> get aug_data_path
+#./logs/Meta-save-acm-num-300-0-20230117-162906-177221
+"
+
+:"
+# step 3
+CUDA_VISIBLE_DEVICES=1 python meta_feat_acc.py --seed=0 --source acm \
+--num_metas 300 --model MLP --hid_dim 128 --encoder_dim 16 \
+--model_path '$??' \
+--aug_data_path './logs/Meta-save-acm-num-300-0-20230117-162906-177221' # use meta-set we have created
+# --> get load_path
+
+# step 4
+CUDA_VISIBLE_DEVICES=1 python meta_regression_nn.py --seed=0 \
+	--source acm --target dblp --model MLP --hid_dim 128 --encoder_dim 16 --val_num 30 --reg_model 'mlp_simple'\
+	--k_laplacian 5 --epochs_reg 200 --lr_reg 0.1 --dropout_reg 0 \
+	--model_path '$??' \
+	--load_path '$??'
+# --> get our 'super model' which is trained based on the original given model
+
+# step 4
+CUDA_VISIBLE_DEVICES=1 python meta_regression_nn.py --seed=0 \
+	--source acm --target dblp --model MLP --hid_dim 128 --encoder_dim 16 --val_num 30 --reg_model 'mlp_simple'\
+	--k_laplacian 5 --epochs_reg 400 --lr_reg 0.1 --dropout_reg 0 \
+	--model_path '$??' \
+	--load_path '$??'
+# --> get our 'super model' which is trained based on the original given model
+"
